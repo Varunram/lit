788fa664ccbab7a50464f07727235abf1d621461
Varunram <vrg2009@ymail.com>
2018-07-06 23:48:16 +0530

    temp

--- qln/netio.go
+++ qln/netio.go
@@ -1,7 +1,7 @@
 package qln
 
 import (
-	"encoding/hex"
+	//"encoding/hex"
 	"fmt"
 	"log"
 	"net"
@@ -21,8 +21,11 @@ func (nd *LitNode) GetLisAddressAndPorts() (
 	var idPub [33]byte
 	copy(idPub[:], idPriv.PubKey().SerializeCompressed())
 
-	lisAdr := hex.EncodeToString(idPub[:])
-
+	lisAdr := lnutil.LitFullKeyAdrEncode(idPub)
+	//log.Println("LLOKING FOR?", lnutil.LitFullKeyAdrEncode(idPub))
+	//x, _ := lnutil.LitFullAdrDecode(lnutil.LitFullKeyAdrEncode(idPub))
+	//log.Println("THIS", x==idPub)
+	//ln1gq87z4dm9d6q03vtlvygct29m5y58ttqgk8rg88wdq0fj8mlcnz5dyzzgu
 	nd.RemoteMtx.Lock()
 	ports := nd.LisIpPorts
 	nd.RemoteMtx.Unlock()
@@ -42,7 +45,7 @@ func (nd *LitNode) TCPListener(
 	var idPub [33]byte
 	copy(idPub[:], idPriv.PubKey().SerializeCompressed())
 
-	adr := hex.EncodeToString(idPub[:])
+	adr := lnutil.LitFullKeyAdrEncode(idPub)
 
 	// Don't announce on the tracker if we are communicating via SOCKS proxy
 	if nd.ProxyURL == "" {
@@ -205,7 +208,7 @@ func (nd *LitNode) GetConnectedPeerList() []PeerInfo {
 		newPeer.PeerNumber = k
 		newPeer.RemoteHost = v.Con.RemoteAddr().String()
 		newPeer.Nickname = v.Nickname
-		newPeer.LitAdr = lnutil.LitAdrFromPubkey(pubArr)
+		newPeer.LitAdr = lnutil.LitFullKeyAdrEncode(pubArr)
 		peers = append(peers, newPeer)
 	}
 	return peers
